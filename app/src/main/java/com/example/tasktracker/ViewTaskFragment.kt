930b0b1fddb443632a7f9f31c012d30f234d5429
Jmolin910 <jamolin@quinnipiac.edu>
2024-04-30 13:52:41 -0400

    Finished help fragment and added toast after marking a task complete.

--- app/src/main/java/com/example/tasktracker/ViewTaskFragment.kt
+++ app/src/main/java/com/example/tasktracker/ViewTaskFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -62,6 +63,8 @@ class ViewTaskFragment : Fragment() {
 
     private fun navigateHome() {
         findNavController().navigate(R.id.action_viewTaskFragment_to_homeFragment)
+        Toast.makeText(activity, "Task Marked Complete", Toast.LENGTH_SHORT).show()
+
     }
 
     override fun onDestroyView() {
